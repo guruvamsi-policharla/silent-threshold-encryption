6f3d8904faf1f54ffdf297ed0c2808a29f03115b
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2025-05-05 19:20:48 -0700

    fixed benched

--- benches/encryption.rs
+++ benches/encryption.rs
@@ -1,35 +1,31 @@
-use ark_ec::pairing::Pairing;
-use ark_poly::univariate::DensePolynomial;
-use ark_std::UniformRand;
 use criterion::{criterion_group, criterion_main, Criterion};
 use silent_threshold_encryption::{
+    crs::CRS,
     encryption::encrypt,
-    kzg::KZG10,
-    setup::{AggregateKey, PublicKey, SecretKey},
+    setup::{AggregateKey, SecretKey},
 };
 
 type E = ark_bls12_381::Bls12_381;
-type Fr = <E as Pairing>::ScalarField;
-type UniPoly381 = DensePolynomial<<E as Pairing>::ScalarField>;
 
 fn bench_encrypt(c: &mut Criterion) {
     let mut rng = ark_std::test_rng();
     let n = 8;
     let t = 2;
-    let tau = Fr::rand(&mut rng);
-    let params = KZG10::<E, UniPoly381>::setup(n, tau.clone()).unwrap();
+    let crs = CRS::<E>::new(n, &mut rng);
 
-    let mut sk: Vec<SecretKey<E>> = Vec::new();
-    let mut pk: Vec<PublicKey<E>> = Vec::new();
+    let sk = (0..n)
+        .map(|_| SecretKey::<E>::new(&mut rng))
+        .collect::<Vec<_>>();
 
-    for i in 0..n {
-        sk.push(SecretKey::<E>::new(&mut rng));
-        pk.push(sk[i].get_pk(0, &params, n))
-    }
+    let pk = sk
+        .iter()
+        .enumerate()
+        .map(|(i, sk)| sk.get_pk(i, &crs, n))
+        .collect::<Vec<_>>();
 
-    let ak = AggregateKey::<E>::new(pk, &params);
+    let agg_key = AggregateKey::<E>::new(pk, &crs);
 
-    c.bench_function("encrypt", |b| b.iter(|| encrypt::<E>(&ak, t, &params)));
+    c.bench_function("encrypt", |b| b.iter(|| encrypt::<E>(&agg_key, t, &crs)));
 }
 
 criterion_group!(benches, bench_encrypt);
--- benches/setup.rs
+++ benches/setup.rs
@@ -1,15 +1,7 @@
-use ark_ec::pairing::Pairing;
-use ark_poly::univariate::DensePolynomial;
-use ark_std::UniformRand;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use silent_threshold_encryption::{
-    kzg::KZG10,
-    setup::{LagrangePowers, SecretKey},
-};
+use silent_threshold_encryption::{crs::CRS, setup::SecretKey};
 
 type E = ark_bls12_381::Bls12_381;
-type Fr = <E as Pairing>::ScalarField;
-type UniPoly381 = DensePolynomial<<E as Pairing>::ScalarField>;
 
 fn bench_setup(c: &mut Criterion) {
     // WARNING: This benchmark will take a very long time. It is only meant to measure the speedup when compared to the faster Lagrange setup
@@ -18,35 +10,12 @@ fn bench_setup(c: &mut Criterion) {
     let mut rng = ark_std::test_rng();
     for size in 3..=7 {
         let n = 1 << size;
-        let tau = Fr::rand(&mut rng);
-        let params = KZG10::<E, UniPoly381>::setup(n, tau.clone()).unwrap();
-
+        let crs = CRS::<E>::new(n, &mut rng);
         let sk = SecretKey::<E>::new(&mut rng);
 
-        group.bench_with_input(BenchmarkId::from_parameter(n), &params, |b, inp| {
+        group.bench_with_input(BenchmarkId::from_parameter(n), &crs, |b, inp| {
             b.iter(|| sk.get_pk(0, &inp, n));
         });
-    }
-
-    group.finish();
-
-    let mut group = c.benchmark_group("Lagrange setup");
-    group.sample_size(10);
-    let mut rng = ark_std::test_rng();
-    for size in 3..=10 {
-        let n = 1 << size; // actually n-1 total parties. one party is a dummy party that is always true
-        let tau = Fr::rand(&mut rng);
-        let lagrange_params = LagrangePowers::<E>::new(tau, n);
-
-        let sk = SecretKey::<E>::new(&mut rng);
-
-        group.bench_with_input(
-            BenchmarkId::from_parameter(n),
-            &lagrange_params,
-            |b, inp| {
-                b.iter(|| sk.lagrange_get_pk(0, &inp, n));
-            },
-        );
     }
 
     group.finish();
